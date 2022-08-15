9c29e85448cafc84f055e8a2bb1481041e194f8f
GemBox Dev Team <info@gemboxsoftware.com>
2022-08-15 07:27:47 +0200

    Updated "Save" example.

--- C#/Common Uses/Save/Program.cs
+++ C#/Common Uses/Save/Program.cs
@@ -1,8 +1,16 @@
+using System.IO;
+using System.IO.Compression;
 using GemBox.Email;
 
 class Program
 {
     static void Main()
+    {
+        Example1();
+        Example2();
+    }
+
+    static void Example1()
     {
         // If using Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY");
@@ -20,5 +28,27 @@ class Program
 
         // Save message to email file.
         message.Save("Save.eml");
+    }
+
+    static void Example2()
+    {
+        // If using Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+        var message = MailMessage.Load("Attachment.eml");
+        string zipName = Path.ChangeExtension("Attachment.eml", ".zip");
+
+        using (var archiveStream = File.OpenWrite(zipName))
+        using (var archive = new ZipArchive(archiveStream, ZipArchiveMode.Create))
+        {
+            foreach (var attachment in message.Attachments)
+            {
+                string attachmentName = attachment.FileName ?? Path.GetRandomFileName();
+                var entry = archive.CreateEntry(attachmentName);
+
+                using (var entryStream = entry.Open())
+                    attachment.Save(entryStream);
+            }
+        }
     }
 }
--- VB.NET/Common Uses/Save/Program.vb
+++ VB.NET/Common Uses/Save/Program.vb
@@ -1,9 +1,17 @@
+Imports System.IO
+Imports System.IO.Compression
 Imports GemBox.Email
 
 Module Program
 
     Sub Main()
 
+        Example1()
+        Example2()
+
+    End Sub
+
+    Sub Example1()
         ' If using Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
@@ -21,4 +29,28 @@ Module Program
         ' Save message to email file.
         message.Save("Save.eml")
     End Sub
+
+    Sub Example2
+        ' If using Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+        Dim message = MailMessage.Load("Attachment.eml")
+        Dim zipName As String = Path.ChangeExtension("Attachment.eml", ".zip")
+
+        Using archiveStream = File.OpenWrite(zipName)
+            Using archive = New ZipArchive(archiveStream, ZipArchiveMode.Create)
+
+                For Each attachment In message.Attachments
+                    Dim attachmentName As String = If(attachment.FileName, Path.GetRandomFileName())
+                    Dim entry = archive.CreateEntry(attachmentName)
+
+                    Using entryStream = entry.Open()
+                        attachment.Save(entryStream)
+                    End Using
+                Next
+
+            End Using
+        End Using
+    End Sub
+
 End Module
