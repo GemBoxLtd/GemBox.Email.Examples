6afe67c6117ff1a49f18cb9b65c2c2b3756fcdba
Mario at GemBox <mario.zorica@gemboxsoftware.com>
2024-06-10 08:24:15 +0200

    Updated "Listing Messages" and "Sending Messages" examples.

--- VB.NET/Exchange/Sending Messages/Program.vb
+++ VB.NET/Exchange/Sending Messages/Program.vb
@@ -1,35 +1,68 @@
 Imports GemBox.Email
 Imports GemBox.Email.Exchange
-Imports System
 
 Module Program
 
     Sub Main()
+        Example1()
+        Example2()
+    End Sub
 
+    Sub Example1()
         ' If using the Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
-        ' Create a new email message.
-        Dim message As New MailMessage(
-            New MailAddress("sender@example.com", "Sender"),
-            New MailAddress("first.receiver@example.com", "First receiver"),
-            New MailAddress("second.receiver@example.com", "Second receiver"))
+        ' Create a new Exchange client.
+        Dim exchangeClient = New ExchangeClient("<HOST> (e.g. https://outlook.office365.com/EWS/Exchange.asmx)")
+        exchangeClient.Authenticate("<USERNAME>", "<PASSWORD>")
 
-        ' Add additional receivers.
-        message.Cc.Add(
-            New MailAddress("third.receiver@example.com", "Third receiver"),
-            New MailAddress("fourth.receiver@example.com", "Fourth receiver"))
+        ' List the first ten messages in the Inbox folder.
+        Dim messageInfos = exchangeClient.ListMessages("Inbox", 0, 10)
 
-        ' Add subject and body.
-        message.Subject = "Send Email in C# and VB.NET"
-        message.BodyText = "Hi 👋," & vbLf &
-            "This message was created and sent with GemBox.Email." & vbLf &
-            "Read more about it on https://www.gemboxsoftware.com/email"
+        For Each messageInfo As ExchangeMessageInfo In messageInfos
+            If Not messageInfo.IsRead Then
 
-        ' Create a new Exchange client and send an email message.
+                ' Get the full mail message.
+                Dim mailMessage = exchangeClient.GetMessage(messageInfo.ExchangeMessageId)
+                Console.WriteLine("From: " + String.Join(", ", mailMessage.From))
+                Console.WriteLine(mailMessage.BodyHtml)
+                Console.WriteLine()
+
+                ' Mark message as read.
+                exchangeClient.MarkMessageAsRead(messageInfo.ExchangeMessageId)
+            End If
+        Next
+    End Sub
+
+    Sub Example2()
+        ' If using the Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+        ' Create new email message.
+        Dim message As New MailMessage("sender@example.com", "receiver@example.com")
+
+        ' Add subject.
+        message.Subject = "Send HTML Email with Image and Attachment"
+
+        ' Add HTML body with CID embedded image.
+        Dim cid As String = "image001@gembox.com"
+        message.BodyHtml = "<html><body>" &
+            "<p>Hi 👋,</p>" &
+            "<p>This message was created and sent with:</p>" &
+            "<p><img src='cid:" & cid & "'/></p>" &
+            "<p>Read more about it on <a href='https://www.gemboxsoftware.com/email'>GemBox.Email Overview</a> page.</p>" &
+            "</body></html>"
+
+        ' Add image as inline attachment.
+        message.Attachments.Add(New Attachment("%#GemBoxEmailLogo.png%") With {.ContentId = cid})
+
+        ' Add file as attachment.
+        message.Attachments.Add(New Attachment("%#GemBoxSampleFile.pdf%"))
+
+        ' Create a new Exchange client and send the email message.
         Dim exchangeClient = New ExchangeClient("<HOST> (e.g. https://outlook.office365.com/EWS/Exchange.asmx)")
         exchangeClient.Authenticate("<USERNAME>", "<PASSWORD>")
         exchangeClient.SendMessage(message)
-
     End Sub
+
 End Module
