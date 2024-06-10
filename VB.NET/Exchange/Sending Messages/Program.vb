3c3507f6d25a2910186e0ee916e5fcebf31041f6
Mario at GemBox <mario.zorica@gemboxsoftware.com>
2024-06-10 09:08:21 +0200

    Updated "Sending Messages" example.

--- VB.NET/Exchange/Sending Messages/Program.vb
+++ VB.NET/Exchange/Sending Messages/Program.vb
@@ -12,26 +12,27 @@ Module Program
         ' If using the Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
-        ' Create a new Exchange client.
+        ' Create a new email message.
+        Dim message As New MailMessage(
+            New MailAddress("sender@example.com", "Sender"),
+            New MailAddress("first.receiver@example.com", "First receiver"),
+            New MailAddress("second.receiver@example.com", "Second receiver"))
+
+        ' Add additional receivers.
+        message.Cc.Add(
+            New MailAddress("third.receiver@example.com", "Third receiver"),
+            New MailAddress("fourth.receiver@example.com", "Fourth receiver"))
+
+        ' Add subject and body.
+        message.Subject = "Send Email in C# and VB.NET"
+        message.BodyText = "Hi 👋," & vbLf &
+            "This message was created and sent with GemBox.Email." & vbLf &
+            "Read more about it on https://www.gemboxsoftware.com/email"
+
+        ' Create a new Exchange client and send an email message.
         Dim exchangeClient = New ExchangeClient("<HOST> (e.g. https://outlook.office365.com/EWS/Exchange.asmx)")
         exchangeClient.Authenticate("<USERNAME>", "<PASSWORD>")
-
-        ' List the first ten messages in the Inbox folder.
-        Dim messageInfos = exchangeClient.ListMessages("Inbox", 0, 10)
-
-        For Each messageInfo As ExchangeMessageInfo In messageInfos
-            If Not messageInfo.IsRead Then
-
-                ' Get the full mail message.
-                Dim mailMessage = exchangeClient.GetMessage(messageInfo.ExchangeMessageId)
-                Console.WriteLine("From: " + String.Join(", ", mailMessage.From))
-                Console.WriteLine(mailMessage.BodyHtml)
-                Console.WriteLine()
-
-                ' Mark message as read.
-                exchangeClient.MarkMessageAsRead(messageInfo.ExchangeMessageId)
-            End If
-        Next
+        exchangeClient.SendMessage(message)
     End Sub
 
     Sub Example2()
